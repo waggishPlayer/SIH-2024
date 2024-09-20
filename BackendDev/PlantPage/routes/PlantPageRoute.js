8503fe57171a412e64f78d24bbed494220927dfe
waggishPlayer <ali.accessideas@gmail.com>
2024-09-20 12:57:00 +0530

    testing

--- BackendDev/PlantPage/routes/PlantPageRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/SpecificPlant')
-
-//Routes
-router.get('/plant/:plantID',  controller.SimilarPlants)
-
-module.exports = router
--- BackendDev/Routes/PlantDatabase.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../Controllers/Database')
-
-//Routes
-router.post('/AddPlant',  controller.AddPlants)
-
-module.exports = router
